d75cd0a9138080880574df1f730a1dfb2b1d8021
Denys48 <kinzerskyi132@gmail.com>
2025-10-27 00:14:03 +0200

    add task 1

---
+++ homework_3/task1_data_types/main.js
@@ -0,0 +1,9 @@
+console.log(typeof 0);
+console.log(typeof "0");
+console.log(typeof undefined);
+console.log(typeof null); //виведе object, хоч null є окремим типом даних
+console.log(typeof true);
+console.log(typeof Symbol("description"));
+console.log(typeof 42n);
+console.log(typeof [1, 2, 3]);
+console.log(typeof function(){}); //також ввважається об'єктом
