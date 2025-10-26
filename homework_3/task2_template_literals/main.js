4e307de8e86ec51bcb0552f74609edeb61d74588
Denys48 <kinzerskyi132@gmail.com>
2025-10-27 00:18:32 +0200

    add task 2

---
+++ homework_3/task2_template_literals/main.js
@@ -0,0 +1,5 @@
+const name = prompt("Enter your name:", "John");
+const quantity = prompt("How many balloons you would like to order?", "1");
+const color = prompt("Which color do you want your balloons?", "red");
+
+console.log(`Hello ${name}! Your order of ${quantity} ${color} balloon(s) will be ready by tomorrow.`);
