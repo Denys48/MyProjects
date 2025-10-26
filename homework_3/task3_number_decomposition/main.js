feb27470f79fc1bb806dea581d0c3d1e5638c033
Denys48 <kinzerskyi132@gmail.com>
2025-10-27 00:55:27 +0200

    add task 3

---
+++ homework_3/task3_number_decomposition/main.js
@@ -0,0 +1,6 @@
+const number1 = 10369;
+const strNumber = String(number1);
+console.log(strNumber[0], strNumber[1], strNumber[2], strNumber[3], strNumber[4]);
+
+const number2 = 23897;
+console.log(String(number2).split("").join(" "));
